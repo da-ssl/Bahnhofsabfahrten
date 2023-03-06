4ccb7ec7b86865b92d7d20eb64949a1e37673577
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2023-03-06 19:17:44 +0100

    Update [departures].js

--- src/pages/departures/[departures].js
+++ src/pages/departures/[departures].js
@@ -4,13 +4,12 @@ import styles from '@/styles/Departures.module.css'
 import Header from '../../../components/Header/header'
 import Loader from '../../../components/Loader/loader'
 const inter = Inter({ subsets: ['latin'] })
-const baseurl = "http://127.0.0.1:3000"
 export const getServerSideProps = async ({params}) => {
   const getparams = {params}
   console.log(getparams)
   const getstation = params['departures']
   console.log(getstation)
-  const res  = await fetch(baseurl + "/api/station/" + getstation)
+  const res  = await fetch("/api/station/" + getstation)
   const data = await res.json()
   const stationname = data['station']['name']
   return{
